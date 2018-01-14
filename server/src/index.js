2675f8c3d4cbc8fd2f4099f4d89714333c179070
Hugo <hugo@thegonzalez.design>
2018-01-14 12:31:51 -0800

    renamed public to source and fixed the path to the index

--- server/src/index.js
+++ server/src/index.js
@@ -4,17 +4,17 @@ const cors = require('cors')
 const morgan = require('morgan')
 const server = express()
 
-const public = '../client/public/'
+const source = '../client/public/'
 
 server.use(morgan('combined'))
 server.use(bodyParser.json())
 server.use(cors())
-server.use(express.static(public))
+server.use(express.static(source))
 server.set('trust proxy', true)
 server.set('trust proxy', 'loopback')
 
 server.get('/', (req, res) => {
-	res.sendFile(__dirname + '/' + public + 'index.html')
+	res.sendFile(source + 'index.html')
 })
 
 server.get('/getData', (req, res) => {
