b3aaf3993ebc1b221e2963b60b56e422a9917940
Hugo <hugo@thegonzalez.design>
2018-01-26 08:27:17 -0800

    stripe.js

---
+++ server/src/stripe.js
@@ -0,0 +1,15 @@
+exports.charge = (amount = 1000, currency = 'usd', source) => {
+	const {
+		secret_key
+	} = require('./dynamix')
+	const stripe = require("stripe")(secret_key);
+	stripe.charges.create({
+		amount: amount,
+		currency: currency,
+		source: source,
+	}, function (err, charge) {
+		// asynchronously called
+		console.error('Error:', Error)
+		console.warn('Charge:', charge)
+	});
+}
