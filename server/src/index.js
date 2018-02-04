c5747e8716bdee21cbf04884009eb3614e0dabb5
Hugo <hugo@thegonzalez.design>
2018-02-04 01:41:13 -0800

    fixed stripe payment system

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const dynamix = require('./dynamix')
+const stripe = require("stripe")(dynamix.secret_key);
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
@@ -6,9 +7,6 @@ const cors = require('cors')
 const {
 	cartel
 } = require('./cartel')
-const {
-	charge
-} = require('./stripe')
 
 const source = path.resolve(__dirname, '../../client/public/')
 const index = path.join(source, '/index.html')
@@ -35,11 +33,28 @@ server.post('/', async (req, res) => {
 })
 server.post('/charge', async (req, res) => {
 	console.log('Recived POST (/Charge)\n', req.body)
-	const token = req.body.stripeToken
-	console.log('Token:', token)
-	charge({
-		source: token
-	})
+	const {token, order, email} = await req.body
+	await stripe.charges.create({
+		amount: (order.total * 100),
+		currency: 'usd',
+		source: token,
+		receipt_email: email,
+		description: "Your order"
+	}, function (err, charge) {
+		// asynchronously called
+		if (err) {
+			console.error('Error:', err.message)
+			console.warn('Charge:', charge)
+			console.log('source:', token)
+			console.log('amount:', order.total)
+			console.log('email:', email)
+			res.send(false)
+		}else {
+ 			console.warn('Charge:', charge)
+			if (charge.paid) res.send(true)
+			else res.send(false)
+		}
+	});
 })
 
 server.listen(port, console.warn(`Listening on ${port}`))
