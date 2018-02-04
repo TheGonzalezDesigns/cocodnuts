dd347e49c1191d57fae4ca36381c1f9748deda81
Hugo <hugo@thegonzalez.design>
2018-02-04 00:07:40 -0800

    updated stripe comp

--- client/src/scripts/api/stripe.js
+++ client/src/scripts/api/stripe.js
@@ -1,10 +1,9 @@
 /*global Stripe*/
 import {key, style} from './stripeConfig'
 
+const stripe = Stripe(key)
 exports.start = () => {
-	const stripe = Stripe(key)
 	const elements = stripe.elements()
-
 	const card = elements.create('card', {style})
 
 	card.mount('#card-element')
@@ -17,27 +16,11 @@ exports.start = () => {
 		if (complete) stripeButton.disabled = false
 	})
 
-	const stripeTokenHandler = (token) => {
-		const form = document.getElementById('payment-form')
-		const hiddenInput = document.createElement('input')
-		hiddenInput.setAttribute('type', 'hidden')
-		hiddenInput.setAttribute('name', 'stripeToken')
-		hiddenInput.setAttribute('value', token.id)
-		form.appendChild(hiddenInput)
-		//form.submit()
-		console.log('token', token)
-	}
 	const form = document.getElementById('payment-form')
 	form.addEventListener('submit', async (event) => {
 		event.preventDefault()
-
-		const {token, error} = await stripe.createToken(card)
-
-		if (error) {
-			const errorElement = document.getElementById('card-errors')
-			errorElement.textContent = error.message
-		} else {
-			stripeTokenHandler(token)
-		}
 	})
+
+	return card
 }
+exports.stripe = stripe
