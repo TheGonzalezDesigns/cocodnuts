38a0361b2f80eb2127aab2af12baedd054421d6e
Hugo <hugo@thegonzalez.design>
2018-02-03 20:01:56 -0800

    swapped lineheight in favor of paddingTop

--- client/src/scripts/api/stripeConfig.js
+++ client/src/scripts/api/stripeConfig.js
@@ -1,15 +1,15 @@
-/*global exports*/
 exports.key = 'pk_test_jU1S9e02WwQKSuzyP2KITo9X'
 exports.style = {
 	base: {
 		color: '#32325d',
-		lineHeight: '18px',
+		paddingTop: '18px',
 		fontFamily: '"Roboto Slab", sans-serif',
 		fontSmoothing: 'antialiased',
 		fontSize: '16px',
 		'::placeholder': {
 			color: '#aab7c4'
-		}
+		},
+		background: 'rgba(255,255,255,0.2)'
 	},
 	invalid: {
 		color: '#fa755a',
