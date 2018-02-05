338b4c1ec60009eaa60b637091e730aa703bb459
Hugo <hugo@thegonzalez.design>
2018-02-05 01:06:22 -0800

    added correct email credentials

--- server/src/dynamix.js
+++ server/src/dynamix.js
@@ -1,2 +1,6 @@
 exports.port = 8080
 exports.secret_key = "sk_test_p0DxDJ8ETkeOiJM2bqkCVFYb"
+exports.password = 'yummy2018'
+exports.email = 'cocodnutsorder@gmail.com'
+exports.name = 'rob otto'
+exports.chef = 'cocodnuts@gmail.com'
