444cb59729e874e0bb56525ab8e3b392db3baecc
Hugo <hugo@thegonzalez.design>
2018-02-06 22:19:09 -0800

    added more optimizations for webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,14 @@
 /*global require __dirname module*/
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const freindlyFormatter = require('eslint-friendly-formatter')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const poststylus = require('poststylus')
-const webpack = require('webpack')
 const path = require('path')
-
+const glob = require('glob')
+const webpack = require('webpack')
+const poststylus = require('poststylus')
+const WebpackMonitor = require('webpack-monitor')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PurifyCSSPlugin = require('purifycss-extended-webpack')
+const freindlyFormatter = require('eslint-friendly-formatter')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const PATHS = {
 	src: path.join(__dirname, '/src'),
 	dist: path.join(__dirname, '/public'),
@@ -13,6 +16,7 @@ const PATHS = {
 const FILES = {
 	entry: path.join(PATHS.src, '/scripts/index.js'),
 	template: path.join(PATHS.src, '/pug/index.pug'),
+	html: path.join(PATHS.dist, '/index.html'),
 }
 
 module.exports = {
@@ -62,6 +66,7 @@ module.exports = {
 				use: [
 					'responsive-loader',
 					'file-loader?name=graphics/[name].[ext]',
+					'img-loader'
 				],
 			},
 			{
@@ -77,6 +82,27 @@ module.exports = {
 			options: {
 				stylus: {
 					use: [poststylus(['autoprefixer', 'rucksack-css'])],
+				},
+				img: {
+					enabled: process.env.NODE_ENV === 'production',
+          gifsicle: {
+            interlaced: false
+          },
+          mozjpeg: {
+            progressive: true,
+            arithmetic: false
+          },
+          optipng: false, // disabled
+          pngquant: {
+            floyd: 0.5,
+            speed: 2
+          },
+          svgo: {
+            plugins: [
+              { removeTitle: true },
+              { convertPathData: false }
+            ]
+          }
 				}
 			},
 		}),
@@ -95,15 +121,28 @@ module.exports = {
 			'process.env': {
 				NODE_ENV: JSON.stringify('production')
 			}
-		})
+		}),
+		new PurifyCSSPlugin({
+      paths: glob.sync(FILES.template),
+			minimize: true,
+			purifyOptions: {
+				minify: true
+			}
+    }),
+		new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: {removeAll: true } },
+      canPrint: true
+    }),
+		new WebpackMonitor({
+	    capture: true, // -> default 'true'
+	    target: '../monitor/myStatsStore.json', // default -> '../monitor/stats.json'
+	    launch: true, // -> default 'false'
+	    port: 3030, // default -> 8081
+	    excludeSourceMaps: true // default 'true'
+	  })
 	],
-	devServer: {
-		contentBase: PATHS.dist,
-		compress: true,
-		port: 3001,
-		//stats: 'minimal',
-		open: true,
-	},
 	resolve: {
 		alias: {
 			vue: 'vue/dist/vue.js',
