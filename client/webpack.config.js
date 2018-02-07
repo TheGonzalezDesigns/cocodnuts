06576635980e6206cf019dd0b2f1fc807fd5d5b3
Hugo <hugo@thegonzalez.design>
2018-02-06 20:18:00 -0800

    added production mode for view

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -90,6 +90,11 @@ module.exports = {
 				collapseWhitespace: true,
 			},
 			hash: true,
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
 		})
 	],
 	devServer: {
